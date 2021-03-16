69c50217aadba7b2a33bcd0e2f86422af99aaa52
Simon <simon.moro@imt-atlantique.net>
2021-03-16 16:09:01 +0100

    Infos dans le form persistent si erreur lors du submit

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ with app.test_request_context():
 @app.route('/')
 def index():
     posts = database.models.Post.query.all()
-    print(posts)
     return flask.render_template("homepage.html.jinja2",
                                  posts=posts)
 
@@ -33,27 +32,27 @@ def create_or_process_post(post_id=None):
     form = request.form
     #form = PostEditForm(obj=post)
     if request.method=='POST':
-        file = request.files['file']
-        if file.filename=='':
-            file=None
-    else :
-        file=None
-    if file != None:
         if post is None:
             post = database.models.Post()
         post.user_id = 1
-        post.title = form.get("title","")
-        post.content = form.get("description","")
-        #print(post.title)
-        #print(post.content)
-        post.image_data = base64.b64encode(file.read())
-        #print(post.image_data)
-        db.session.add(post)
-        db.session.commit()
+        post.title = form.get("title")
+        post.content = form.get("description")
+        post.tags = form.get("tags")
+        file = request.files['file']
+        if file.filename=='':
+            file=None
+        else :
+            file=None
+        if file != None:
+
+            post.image_data = base64.b64encode(file.read())
+            db.session.add(post)
+            db.session.commit()
 
 
 
-        return flask.redirect(flask.url_for('index'))
+            return flask.redirect(flask.url_for('index'))
+        return flask.render_template('edit_post_form.html.jinja2', form=form, post=post)
     else:
         return flask.render_template('edit_post_form.html.jinja2', form=form, post=post)
 
@@ -64,20 +63,20 @@ def create_or_process_post(post_id=None):
 def comment_a_post(comment_id=None,post_id=None):
     comment = database.models.Comment.query.filter_by(id=comment_id).first()
     form = request.form
-    if request.method=='POST':
-        if comment is None:
-            comment = database.models.Comment()
+    content = form.get("description")
+    if content!="":
+        if request.method=='POST':
+            if comment is None:
+                comment = database.models.Comment()
+            comment.content = content
+            comment.user_id = 1
+            comment.post_id = post_id
+            post = database.models.Post.query.filter_by(id=post_id).first()
+            comment.post=post # permet de gérer le backpopulates de "post.comments"
+            db.session.add(comment)
+            db.session.commit()
 
-        comment.user_id = 1
-        comment.post_id = form.get("post_id")
-        comment.content = form.get("description")
-        post = database.models.Post.query.filter_by(id=post_id).first()
-        comment.post=post # permet de gérer le backpopulates de "post.comments"
-        db.session.add(comment)
-        db.session.commit()
-
-        return flask.redirect(flask.url_for('index'))
-    #return flask.render_template('comment_post.html.jinja2', post_id=post_id,form=form, comment=comment)
+    return flask.redirect(flask.url_for('index'))
 
 
 @app.route("/posts/delete/<post_id>")
