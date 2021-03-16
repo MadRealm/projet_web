64154af52cb2a0099932cde36db403ddcd01c7a7
MadRealm <62743585+MadRealm@users.noreply.github.com>
2021-03-16 16:55:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,68 @@ def load_user(user):
 @app.route('/')
 def index():
     posts = database.models.Post.query.all()
-	@@ -98,100 +82,18 @@ def delete_post(post_id=None):
+    return flask.render_template("homepage.html.jinja2",
+                                 posts=posts)
+
+
+@app.route("/posts/edit/", methods=["GET", "POST"])
+@app.route("/posts/edit/<post_id>", methods=["GET", "POST"])
+def create_or_process_post(post_id=None):
+    # Fetch the corresponding post from the database. If no ID is provided,
+    # then post will be 'None', and the form will consider this value
+    # as a sign that a new post should be created
+    post = database.models.Post.query.filter_by(id=post_id).first()
+    form = request.form
+    #form = PostEditForm(obj=post)
+    if request.method=='POST':
+        file = request.files['file']
+        if file.filename=='':
+            file=None
+    else :
+        file=None
+    if file != None:
+        if post is None:
+            post = database.models.Post()
+        post.user_id = 1
+        post.title = form.get("title","")
+        post.content = form.get("description","")
+        #print(post.title)
+        #print(post.content)
+        post.image_data = base64.b64encode(file.read())
+        #print(post.image_data)
+        db.session.add(post)
+        db.session.commit()
+
+
+
+        return flask.redirect(flask.url_for('index'))
+    else:
+        return flask.render_template('edit_post_form.html.jinja2', form=form, post=post)
+
+@app.route("/comment/", methods=["GET", "POST"])
+@app.route("/comment/<post_id>", methods=["GET", "POST"])
+#@app.route("/comment/<post_id>/<comment_id>", methods=["GET", "POST"])
+def comment_a_post(comment_id=None,post_id=None):
+    comment = database.models.Comment.query.filter_by(id=comment_id).first()
+    form = request.form
+    if request.method=='POST':
+        if comment is None:
+            comment = database.models.Comment()
+
+        comment.user_id = 1
+        comment.post_id = form.get("post_id","")
+        comment.content = form.get("description","")
+        db.session.add(comment)
+        db.session.commit()
+
+        return flask.redirect(flask.url_for('index'))
+    return flask.render_template('comment_post.html.jinja2', post_id=post_id,form=form, comment=comment)
+
+
+@app.route("/posts/delete/<post_id>")
+def delete_post(post_id=None):
+    post = database.models.Post.query.filter_by(id=post_id).first()
+    db.session.delete(post)
     db.session.commit()
     return flask.redirect(flask.url_for('index'))\
 
